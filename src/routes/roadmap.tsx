bb04b1c1986c5a78961a8cd84a134f394063d706
KENEW <creeter@naver.com>
2023-03-11 03:08:40 +0900

    이미지 불러오기 방식 import로 변경 (테스트)

--- src/routes/roadmap.tsx
+++ src/routes/roadmap.tsx
@@ -1,15 +1,20 @@
 import { RoadmapContent, RoadmapComponent, RoadmapMedia } from '@/components/roadmapComponent'
 import { Layout } from '@/components/layout'
+import roadmapImg from '/public/images/Roadmap.png';
 
 export default function RoadmapPage() {
   return (
     <Layout>
       <RoadmapComponent>
         <RoadmapContent title="SNEAK-PIC">
-        <img src='public\images\Roadmap.png' className="sneakPic"/>
-        
+          <img src={roadmapImg} className="sneakPic"/>
         </RoadmapContent>
       </RoadmapComponent>
     </Layout>
   )
 }
+
+
+//<img src={require("public/images/Roadmap.png").default} className="sneakPic"/>
+
+//<img src="/public/images/Roadmap.png" className="sneakPic"/>
